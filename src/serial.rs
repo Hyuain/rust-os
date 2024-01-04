6f0a9736dde10d35dc000bb402d8e1734c576a07
harvey <hyuain97@163.com>
2024-01-04 15:14:37 +0800

    print to console through serial port

---
+++ src/serial.rs
@@ -0,0 +1,39 @@
+use core::fmt;
+use core::fmt::Write;
+use uart_16550::SerialPort;
+use spin::Mutex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    pub static ref SERIAL1: Mutex<SerialPort> = {
+        // UART needs multiple I/O ports. we pass the first port to it, and it will calc all needed ports
+        // 0x3F8 is the standard port number for the first serial interface
+        let mut serial_port = unsafe { SerialPort::new(0x3F8) };
+        serial_port.init();
+        Mutex::new(serial_port)
+    };
+}
+
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_print(format_args!($($arg)*));
+    };
+}
+
+#[macro_export]
+macro_rules! serial_println {
+    () => {
+        $crate::serial_print!("\n")
+    };
+    ($fmt:expr) => {
+        $crate::serial_print!(concat!($fmt, "\n"))
+    };
+    ($fmt:expr, $($arg:tt)*) => {
+        $crate::serial_print!(concat!($fmt, "\n"), $($arg)*)
+    };
+}
+
+pub fn _print(args: fmt::Arguments) {
+    SERIAL1.lock().write_fmt(args).expect("Print to serial failed");
+}
