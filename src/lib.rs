6b6eb1d54edd2fe377c134bf614b66bbdf815bb8
harvey <hyuain97@163.com>
2024-01-07 10:40:32 +0800

    add a breakpoint interrupts handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
@@ -9,6 +10,7 @@ use x86_64::instructions::port::Port;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -75,3 +77,6 @@ fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
 }
 
+pub fn init() {
+    interrupts::init_idt();
+}
