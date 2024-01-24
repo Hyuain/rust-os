249632418baa9e9c48bd583086eec5c5ccdbfe59
harvey <hyuain97@163.com>
2024-01-24 11:11:34 +0800

    init heap memory mapping and linked_list_allocator

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,16 @@
 extern crate alloc;
 
 use alloc::boxed::Box;
+use alloc::rc::Rc;
+use alloc::vec;
+use alloc::vec::Vec;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use x86_64::structures::paging::{Page, PageTable, Translate};
 use x86_64::VirtAddr;
 
 use rust_os::memory::BootInfoFrameAllocator;
-use rust_os::{memory, print, println};
+use rust_os::{allocator, memory, println};
 
 entry_point!(kernel_main);
 
@@ -23,22 +26,43 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     rust_os::init();
 
-    /* Test memory mapping */
-
+    /* Init mapper, level-4 page table instance and other memory related things */
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
     // let mut frame_allocator = memory::EmptyFrameAllocator;
 
+    /* Test paging and memory mapping */
     let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
     memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
-    /* Test heap allocation */
+    /* Init heap allocator */
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    let x = Box::new(41);
+    /* Test heap allocation */
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+    // create a dynamically sized vector
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec slice at {:p}", vec.as_slice());
+    // create a reference counted vector -> will be freed when count reaches 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     // invoke a breakpoint exception
     // x86_64::instructions::interrupts::int3();
