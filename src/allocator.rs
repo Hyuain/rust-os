06b47b69fadde687ae3c76b3e0c2e5746dd4c8cd
harvey <hyuain97@163.com>
2024-01-25 11:22:29 +0800

    linked list allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,8 @@
 pub mod bump;
+pub mod linked_list;
 
 use crate::allocator::bump::{BumpAllocator, Locked};
+use crate::allocator::linked_list::LinkedListAllocator;
 use core::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
 use x86_64::structures::paging::mapper::MapToError;
@@ -10,7 +12,7 @@ use x86_64::VirtAddr;
 pub struct Dummy;
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
